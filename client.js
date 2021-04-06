a7ae222784abb77dba2ff76d03c3b097d0c16163
Luke Penner <lukepenner@hotmail.ca>
2021-04-05 18:21:18 -0700

    setup input added

--- client.js
+++ client.js
@@ -17,4 +17,19 @@ const connect = function() {
   return conn;
 };
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+  return stdin;
+};
+
+setupInput();
+
 module.exports = {connect};
